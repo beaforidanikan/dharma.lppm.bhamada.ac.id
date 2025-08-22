0f6743f11d11eba78edd40a9c601507204e8f9c2
beaforidanikan <beaforidanikan@gmail.com>
2025-08-22 14:50:52 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,21 @@
 <?php
 
-define('LARAVEL_START', microtime(true));
+/**
+ * Laravel - A PHP Framework For Web Artisans
+ *
+ * @package  Laravel
+ * @author   Taylor Otwell <taylor@laravel.com>
+ */
 
-require __DIR__.'/../vendor/autoload.php';
-
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
+$uri = urldecode(
+    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
 );
 
-$response->send();
+// This file allows us to emulate Apache's "mod_rewrite" functionality from the
+// built-in PHP web server. This provides a convenient way to test a Laravel
+// application without having installed a "real" web server software here.
+if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
+    return false;
+}
 
-$kernel->terminate($request, $response);
+require_once __DIR__.'/public/index.php';
